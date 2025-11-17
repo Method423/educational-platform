38dbabc52a45ea54f8dd54a2f5e4c58049c6481a
Method423 <neo.matrix.1994@bk.ru>
2025-11-17 16:34:49 +0300

    Merge division branch: add multiplication and division

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ int main() {
     int a, b;
     cout << "Введите A и B: ";
     cin >> a >> b;
+    
     cout << "A+B=" << a + b << endl;
     cout << "A-B=" << a - b << endl;
-<<<<<<< HEAD
-   cout << "A*B=" << a * b << endl;
+    cout << "A*B=" << a * b << endl;
     
     if (b != 0) {
         cout << "A/B=" << a / b << endl;
@@ -16,6 +16,5 @@ int main() {
         cout << "Деление на ноль!" << endl;
     }
     
->>>>>>> division
     return 0;
 }
