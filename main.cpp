38fa638891903bd98006dbcb3f832a16824613eb
Method423 <neo.matrix.1994@bk.ru>
2025-11-17 16:28:49 +0300

    Add division with error handling

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "sdt.h"
 
+// Учебная программа под системой контроля версий
 int main() {
     int a, b;
     cout << "Введите A и B: ";
     cin >> a >> b;
     cout << "A+B=" << a + b << endl;
     cout << "A-B=" << a - b << endl;
+    
+    if (b != 0) {
+        cout << "A/B=" << a / b << endl;
+    } else {
+        cout << "Деление на ноль!" << endl;
+    }
+    
     return 0;
 }
